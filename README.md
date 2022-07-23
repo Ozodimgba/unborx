2eeb7b98b837aaa3a4677bcc71a9123da92da720
Ozodimgba <idasiadiachi@gmail.com>
2022-07-23 21:49:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-epny55
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-epny55)
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { Button } from '@nextui-org/react';
+
+const Component = () => <Button>Click me</Button>;
+
